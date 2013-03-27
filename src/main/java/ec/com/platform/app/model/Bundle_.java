41d61656921a10b6729e0d7e02dc77e3171a7bea
Eduardo Plua Alay <eduardo.plua@gmail.com>
2013-03-26 22:10:23 -0500

    Commit into master
    Change-Id: I526b701416fcb553cce8b1e175963e9aef114c20

---
+++ src/main/java/ec/com/platform/app/model/Bundle_.java
@@ -0,0 +1,17 @@
+package ec.com.platform.app.model;
+
+/**
+ * Entity model attributes for {@link Bundle}
+ * 
+ * @author <a href="mailto:eduardo.plua@gmail.com">Eduardo Plua Alay</a>
+ * @since 2013-03-24
+ */
+public abstract class Bundle_ {
+
+	public static String id = "id";
+	public static String codigo = "codigo";
+	public static String localidad = "localidad";
+	public static String valor = "valor";
+	
+}
+
---
+++ src/main/java/ec/com/platform/app/web/jsf/mb/GenericAppMB.java
@@ -0,0 +1,26 @@
+package ec.com.platform.app.web.jsf.mb;
+
+import javax.faces.bean.ManagedProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import ec.com.platform.app.core.service.AppS;
+import ec.com.platform.generic.model.Generic;
+import ec.com.platform.generic.web.jsf.mb.GenericMB;
+
+/**
+ * @author <a href="mailto:eduardo.plua@gmail.com">Eduardo Plua Alay</a>
+ * @since 2013-03-24 
+ */
+public abstract class GenericAppMB<T extends Generic<T>> extends GenericMB<T> {
+
+	private static final long serialVersionUID = -5560084142240463124L;
+
+	public GenericAppMB() {
+        super();
+    }
+	
+	@Getter @Setter
+    @ManagedProperty(value = "#{appS}")
+    protected AppS appS;
+}
