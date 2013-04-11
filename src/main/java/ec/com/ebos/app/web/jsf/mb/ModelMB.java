56b31989f6360a4b7569503f19054e009567d337
Eduardo Plúa Alay <eduardo.plua@gmail.com>
2013-04-11 17:43:10 -0500

    Agregar dialogs dinámicamente en desk 25%

--- src/main/java/ec/com/ebos/app/web/jsf/mb/ModelMB.java
+++ src/main/java/ec/com/ebos/app/web/jsf/mb/ModelMB.java
@@ -14,17 +14,18 @@ import javax.servlet.http.HttpSession;
 
 import lombok.Setter;
 
+import org.primefaces.component.dialog.Dialog;
 import org.primefaces.component.menuitem.MenuItem;
 import org.primefaces.component.submenu.Submenu;
-import org.primefaces.component.tabview.Tab;
-import org.primefaces.component.tabview.TabView;
 import org.primefaces.context.RequestContext;
+import org.primefaces.event.CloseEvent;
 import org.primefaces.event.TabChangeEvent;
 import org.primefaces.event.TabCloseEvent;
+import org.primefaces.extensions.event.OpenEvent;
 import org.primefaces.model.DefaultMenuModel;
 import org.primefaces.model.MenuModel;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
-import ec.com.ebos.app.web.jsf.component.CustomTab;
 import ec.com.ebos.seguridad.model.Opcion;
 import ec.com.ebos.seguridad.model.RolOpcion;
 
@@ -43,19 +44,15 @@ public class ModelMB implements Serializable{
 	
 	private MenuModel menuModel;
 	
-	@Setter 
-    private TabView tabModel;
-	
-	private CustomTab tab;
-	
 	private List<RolOpcion> rolOpcionList;
 	
-	private String activeOpcionId;
+	private String activeOptionId;
+	
+	private String pngId = "pngDialogs";
 	
 	@PostConstruct
     public void init(){
     	menuModel = new DefaultMenuModel();
-    	tabModel = new TabView();
     }
 	
 	public MenuModel getMenuModel(){
@@ -65,12 +62,6 @@ public class ModelMB implements Serializable{
     	return menuModel;
 	}
 	
-	public TabView getTabModel(){// TODO (epa): quitar si ya no hay errores de CustomTab
-		return tabModel;
-	}
-	
-   
-    
     private void buildMenuModel() { //TODO (epa): Optimizar con Mapas y hacerlo recursivo, con N niveles
     	rolOpcionList = sesionUsuario.getRolOpcionList();
     	//Construye arbol de opciones
@@ -94,7 +85,8 @@ public class ModelMB implements Serializable{
                         if (pantallaOp.getPadre() == modulo) {
                             MenuItem item = new MenuItem();                                                                              
                             item.setValue(pantallaOp.getEtiqueta());
-                            item.setOnclick("jsAbrirOpcion('"+pantallaOp.getId()+"','"+pantallaOp.getTarget()+"');");
+                            //item.setOnclick("jsOpenOption('"+pantallaOp.getId()+"','"+pantallaOp.getTarget()+"');");
+                            item.setOnclick("jsOpenOption('"+pantallaOp.getId()+"');");
                             String iconoPantalla = pantallaOp.getIcono();
                             if(iconoPantalla != null && !iconoPantalla.isEmpty()){
                             	item.setStyle("background-image:url('resources/images/" + iconoPantalla + "') !important;"
@@ -110,75 +102,57 @@ public class ModelMB implements Serializable{
                 menuModel.addSubmenu(submenu);
             }
         }
-        //Limpia el tabModel
-        tabModel.getChildren().clear();
     }
     
     @SuppressWarnings("rawtypes")
-	public void abrirOpcion(){
+	public void openOption(){
     	FacesContext context = FacesContext.getCurrentInstance();
 	    Map map = context.getExternalContext().getRequestParameterMap();
-	    activeOpcionId = (String) map.get("opcionId");
-	    int index = 0;
-	    boolean exist = false;
-
-	    for(UIComponent tab: tabModel.getChildren()){
-	    	if(exist = tab.getId().equals("tab_"+activeOpcionId)){
-	    		index = tabModel.getChildren().indexOf(tab); 
-	    		break;
-	    	}
-	    }
-	    
-	    if(!exist){	    	
-	    	Opcion opcion = null;
-	    	for(RolOpcion rolOpcion : rolOpcionList){ //TODO (epa): Despues de la optimizacion de Mapas, se debe actualizar esta busqueda
-	    		Opcion opcionTMP = rolOpcion.getOpcion();
-	    		if(opcionTMP.getId().equals(Long.parseLong(activeOpcionId))){
-	    			opcion = opcionTMP;
-	    			break;
-	    		}
-	    	}
-	    	if(opcion != null){
-	    		tab = new CustomTab();
-		    	tab.setId("tab_"+activeOpcionId);
-		    	tab.setTitle(opcion.getEtiqueta());
-		    	tab.setClosable(true);
-		    	tab.setOpcion(opcion);
-		    	tabModel.getChildren().add(tab);
-		    	index = tabModel.getChildren().indexOf(tab);
-	    	}
-	    }
-    	tabModel.setActiveIndex(index);
+	    activeOptionId = (String) map.get("optionId");
+	        
+    	Opcion opcion = null;
+    	for(RolOpcion rolOpcion : rolOpcionList){
+    		Opcion opcionTMP = rolOpcion.getOpcion();
+    		if(opcionTMP.getId().equals(Long.parseLong(activeOptionId))){
+    			opcion = opcionTMP;
+    			break;
+    		}
+    	}
+    	if(opcion != null){
+    		UIComponent pngDialogs = context.getViewRoot().findComponent(pngId);
+    		
+    		Dialog dialog = new Dialog();
+    		dialog.setId("dlgOption_"+opcion.getId());
+    		dialog.setHeader(opcion.getEtiqueta());
+    		dialog.setVisible(true);
+    		dialog.setMinimizable(true);
+    		dialog.setMaximizable(true);
+    		dialog.setOnHide("openOption(([{name:'optionId', value:"+opcion.getId()+"}]));");
+    		pngDialogs.getChildren().add(dialog);
+    		RequestContext.getCurrentInstance().update(pngId);
+    	}
     }
     
-    public void cerrarOpcion(TabCloseEvent event){
-    	Tab selectedTab = event.getTab();
-    	String opcionId = selectedTab.getId().substring(4);
-    	//Remueve el beanOption de la sesion
-    	for(RolOpcion rolOpcion : rolOpcionList){ //TODO (epa): Despues de la optimizacion de Mapas, se debe actualizar esta busqueda
-    		Opcion opcionTMP = rolOpcion.getOpcion();
-    		if(opcionTMP.getId().equals(Long.parseLong(opcionId))){
+    @SuppressWarnings("rawtypes")
+	public void closeOption(CloseEvent event){
+	    activeOptionId = event.getComponent().getId();
+	    
+    	//Remove the beanOption of the session
+    	for(RolOpcion rolOpcion : rolOpcionList){
+    		Opcion option = rolOpcion.getOpcion();
+    		if(option.getId().equals(Long.parseLong(activeOptionId))){
     			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 				if (session != null) {
-					session.removeAttribute(opcionTMP.getBeanName());
+					session.removeAttribute(option.getBeanName());
 				}
     			break;
     		}
     	}
-    	//Remueve el tab del tabView
-    	tabModel.getChildren().remove(selectedTab);
-    	RequestContext requestContext = RequestContext.getCurrentInstance();
-    	requestContext.execute("jsCerrarOpcion("+opcionId+")");
-    	if(activeOpcionId.equals(opcionId)){
-    		activeOpcionId = tabModel.getChildren().size() > 0? tabModel.getChildren().get(0).getId().substring(4) : "0";
-    		requestContext.execute("jsCambiarOpcion("+activeOpcionId+")");    		
-    	}    	
+    	RequestContext.getCurrentInstance().execute("jsCloseOption("+activeOptionId+")");
     }
 
-    public void cambiarOpcion(TabChangeEvent event) {
-    	Tab selectedTab = event.getTab();
-    	activeOpcionId = selectedTab != null? selectedTab.getId().substring(4) : "0";
-		RequestContext.getCurrentInstance().execute("jsCambiarOpcion("+activeOpcionId+")");
+    public void changeOption(OpenEvent event) {
+		RequestContext.getCurrentInstance().execute("jsChangeOption("+activeOptionId+")");
 	}
     
 }
