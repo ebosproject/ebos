7cd4d588fac093b7261a51517ce482ba4f735139
Eduardo Plúa Alay <eduardo.plua@gmail.com>
2013-10-04 23:20:04 -0500

    Add lombok and primefaces-extensions dependencies into web2 bundle

--- ec.com.ebos.web2/src/main/java/test/HelloBean.java
+++ ec.com.ebos.web2/src/main/java/test/HelloBean.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,14 +15,16 @@ import org.springframework.stereotype.Component;
 @SessionScoped
 public class HelloBean implements Serializable {
     private static final long serialVersionUID = 1L;
+    
+    @Getter @Setter
     private String name = "Amor";
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
+//    
+//    public String getName() {
+//        return name;
+//    }
+//    
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
 }
